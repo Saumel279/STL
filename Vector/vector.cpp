c1bc5018ca8192f34009dcf65733e46fad4e4cd9
Saumel <saumel279@gmail.com>
2024-12-25 21:10:54 +0600

    vector updated

---
+++ Vector/vector.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+}
+
+// vector<int> v;
+
+// for (int i = 0; i < 5; i++)
+// {
+//     int temp;
+//     cin >> temp;
+//     v.push_back(temp);
+// }
+// for (int i = 0; i < 5; i++)
+// {
+//     cout << v[i] << endl;
+// }
+
+// vector<int> v;
+
+//     for(int i = 0; i < n; i++)
+//     {
+//         int temp;
+//         cin >> temp;
+//         v.push_back(temp);
+//     }
+//     for(int i = 0; i < n; i++)
+//     {
+//         cout << v[i] << " ";
+//     }
+
+// int n;
+// cin >> n;
+
+// vector<int> v(n);
+// for (int i = 0; i < n; i++)
+// {
+//     cin >> v[i];
+// }
+// for (int i = 0; i < n; i++)
+// {
+//     cout << v[i] << " ";
+// }
